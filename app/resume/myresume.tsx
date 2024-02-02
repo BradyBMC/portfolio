82f383fcc1e7e86d1fc417d03addeda87c66e841
Brady's Macbook <bradychan01@gmail.com>
2024-02-01 22:30:07 -0800

    remove pdf string

--- app/resume/myresume.tsx
+++ app/resume/myresume.tsx
@@ -3,7 +3,9 @@ import { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.js',import.meta.url,).toString();
+// import "react-pdf/dist/esm/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 export default function PDFViewer() {
   const [numPages, setNumPages] = useState<number>();
@@ -16,7 +18,7 @@ export default function PDFViewer() {
   return (
     <div>
       <Document file="./Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
+        <Page pageNumber={pageNumber} renderTextLayer={false}/>
       </Document>
       <p>
         Page {pageNumber} of {numPages}
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,7 +3,6 @@ import PDFViewer from "./myresume";
 export default function Resume() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-       RESUME
        <PDFViewer />
     </main>
   );
